c4fa8a8f584fa9132bb28bec0551a85fbaf70a35
Andrew Domingues <drewdomi@proton.me>
2023-02-20 10:17:07 -0300

    fix: add animation to nav bar
    initial modal

--- src/js/init.js
+++ src/js/init.js
@@ -1,23 +1,3 @@
-const hamMenuBtn = document.querySelector('.ham-menu')
-const navMenu = document.querySelector('.nav-menu')
-const linkItem = navMenu.querySelectorAll('* a')
+import navMenu from './navBar.js'
 
-function toggleHideMenuClass() {
-  navMenu.classList.toggle('hide')
-}
-
-function toggleMenu() {
-  const hamMenuImg = document.querySelector('.open-btn')
-  const hamMenuCloseImg = document.querySelector('.close-btn')
-  hamMenuImg.classList.toggle('display-none')
-  hamMenuCloseImg.classList.toggle('display-none')
-  toggleHideMenuClass()
-}
-
-hamMenuBtn.addEventListener('pointerdown', toggleMenu)
-
-linkItem.forEach(linkItem => {
-  linkItem.addEventListener('pointerdown', () => {
-   setTimeout(toggleMenu, 500)
-  })
-})
+navMenu()
---
+++ src/js/navBar.js
@@ -0,0 +1,25 @@
+export default function navMenu() {
+  const hamMenuBtn = document.querySelector('.ham-menu')
+  const navMenu = document.querySelector('.nav-menu')
+  const linkItem = navMenu.querySelectorAll('* a')
+
+  function toggleHideMenuClass() {
+    navMenu.classList.toggle('hide')
+  }
+
+  function toggleMenu() {
+    const hamMenuImg = document.querySelector('.open-btn')
+    const hamMenuCloseImg = document.querySelector('.close-btn')
+    hamMenuImg.classList.toggle('display-none')
+    hamMenuCloseImg.classList.toggle('display-none')
+    toggleHideMenuClass()
+  }
+
+  hamMenuBtn.addEventListener('pointerdown', toggleMenu)
+
+  linkItem.forEach((linkItem) => {
+    linkItem.addEventListener('pointerdown', () => {
+      setTimeout(toggleMenu, 500)
+    })
+  })
+}
