ac9864c2ff20056010c6ed35f50d25ed15ce447f
Andrew Domingues <drewdomi@proton.me>
2023-02-20 11:44:57 -0300

    feat: modal open and close

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,24 @@
 export default function modal() {
-  const body = document.querySelector(body)
+  const body = document.querySelector('body')
+  const cards = document.querySelectorAll('.proj-card')
+  const modal = document.querySelector('.modal')
+  const modalCloseBtn = document.querySelector('.close-modal-btn')
+
+  function openModal() {
+    modal.style.display = 'initial'
+    body.style.overflow = 'hidden'
+  }
+
+  function closeModal(event) {
+    if (event.target == modal || event.target == modalCloseBtn) {
+      modal.style.display = 'none'
+      body.style.overflow = 'initial'
+    }
+  }
+
+  cards.forEach((card) => {
+    card.addEventListener('click', openModal)
+  })
+  modal.addEventListener('click', closeModal)
+  modalCloseBtn.addEventListener('click', () => {})
 }
